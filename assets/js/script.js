0e77a9b62d07c422a8ae6a2776f9a9c005a6ddd3
Damien Luzzo <damienluzzo33@gmail.com>
2021-10-04 00:01:24 -0500

    add functionality to dynamically generate elements and API data values

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ var part = 'hourly,minutely'
 var cityInput = document.getElementById('city-name');
 var saveButton = document.getElementById('submit');
 var savedSearches = document.getElementById('saved-searches');
+var displayWeather = document.getElementById('display-weather');
 
 // create function to gather coordinates from the Five Day API
 function getCoordinates() {
@@ -48,32 +49,116 @@ function getWeatherOneDay(latitude, longitude) {
 		if (response.ok) {
             // turn response into json and then
 			response.json().then(function(data) {
-                console.log(data);
                 // get city name
                 console.log(city_name);
                 // get the data points we want
                 // get date (dt), icon (icon), temp (temp), humidity (humidity), wind speed, and uv index (uvi)
                 console.log("current: " + data.current);
                 console.log("humidity: " + data.current.humidity);
+                var currentHumidity = data.current.humidity;
                 console.log("temperature: " + data.current.temp);
+                var currentTemperature = data.current.temp;
                 console.log("uvi: " + data.current.uvi);
+                var currentUVI = data.current.uvi;
                 console.log("date: " + data.current.dt);
+                var currentDate = data.current.dt;
                 console.log("wind speed: " + data.current.wind_speed);
+                var currentWind = data.current.wind_speed;
+                console.log("wind degree: " + data.current.wind_deg);
+                var currentWindDir = data.current.wind_deg;
                 console.log("weather desc: " + data.current.weather[0].description);
+                var currentDescription = data.current.weather[0].description;
                 console.log("weather icon: " + data.current.weather[0].icon);
+                var currentIcon = data.current.weather[0].icon;
                 console.log("weather status: " + data.current.weather[0].main);
+                var currentStatus = data.current.weather[0].main;
+                // create div to hold the current weather results
+                var currentWeatherResults = document.createElement('div');
+                // create bootsrap html object and populate current weather results
+                currentWeatherResults.innerHTML = `
+                <div class="card mb-3">
+                    <div class="column no-gutters">
+                        <div class="card-body d-flex justify-content-center">
+                            <h3 class="card-title">${city_name}</h3>
+                            <p class="card-text"><small class="text-muted">${currentDate}</small></p>
+                            <div class="row">
+                                <div class="d-flex justify-content-center col-5">
+                                    <div>${currentIcon}</div>
+                                    <p>${currentStatus}</p>
+                                </div>
+                                <div class="d-flex justify-content-left col-5">
+                                    <div class="column">
+                                        <div class="row">
+                                            <p class="card-text">${currentTemperature}</p>
+                                            <p class="card-text">${currentUVI}</p>
+                                            <p class="card-text">${currentHumidity}</p>
+                                        </div>
+                                        <div class="row">
+                                            <p class="card-text">${currentWind}</p>
+                                            <p class="card-text">${currentWindDir}</p>
+                                            <p class="card-text">${currentDescription}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+                // append card to the display weather div
+                displayWeather.appendChild(currentWeatherResults);
+                // add bootsrtap classes to daily results div
+                currentWeatherResults.setAttribute("class", "row d-flex justify-content-center");
+                // TODO = Make sure it all works
+                // TODO = Adjust styles and data values to make it work properly
                 // Under the current day, show the 5-day forecast for the city with date, humidity, min and max temp, wind speed,
                 console.log(data.daily)
                 for (let i = 1; i < 6; i++) {
                     console.log(data.daily[i]);
-                    console.log(data.daily[i].humidity);
-                    console.log(data.daily[i].temp.max);
-                    console.log(data.daily[i].temp.min);
-                    console.log(data.daily[i].dt);
-                    console.log(data.daily[i].wind_speed);
-                    console.log(data.daily[i].weather[0].description);
-                    console.log(data.daily[i].weather[0].icon);
-                    console.log(data.daily[i].weather[0].main);
+                    console.log("humidity: " + data.daily[i].humidity);
+                    var dailyHumidity = data.daily[i].humidity;
+                    console.log("max temperature: " + data.daily[i].temp.max);
+                    var dailyMax = data.daily[i].temp.max;
+                    console.log("min temperature: " + data.daily[i].temp.min);
+                    var dailyMin = data.daily[i].temp.min;
+                    console.log("date: " + data.daily[i].dt);
+                    var dailyDate = data.daily[i].dt;
+                    console.log("wind speed: " + data.daily[i].wind_speed);
+                    var dailyWind = data.daily[i].wind_speed;
+                    console.log("weather desc: " + data.daily[i].weather[0].description);
+                    var dailyDescription = data.daily[i].weather[0].description;
+                    console.log("weather icon: " + data.daily[i].weather[0].icon);
+                    var dailyIcon = data.daily[i].weather[0].icon;
+                    console.log("weather status: " + data.daily[i].weather[0].main);
+                    var dailyStatus =  data.daily[i].weather[0].main;
+                    // create div to append to the display weather section
+                    var dailyWeatherResults = document.createElement("div");
+                    // dynamic set inner html to be a bootstrap card with extracted API weather values
+                    dailyWeatherResults.innerHTML = `<div class="card border-success mb-3" style="max-width: 12rem;">
+                        <div class="card-header bg-transparent border-success text-align-center">${dailyDate}</div>
+                        <div class="card-body text-success">
+                            <div class="d-flex justify-content-center"> 
+                                <div>${dailyIcon}</div>
+                                <p class="card-text">${dailyStatus}</p>
+                            </div>
+                            <div>
+                                <div class="row d-flex justify-content-around">
+                                    <p class="card-text">${dailyMin}</p>
+                                    <p class="card-text">${dailyMax}</p>
+                                </div>
+                                <div class="row d-flex justify-content-around">
+                                    <p class="card-text">${dailyWind}</p>
+                                    <p class="card-text">${dailyHumidity}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer bg-transparent border-success d-flex justify-content-center text-align-center">
+                            ${dailyDescription}
+                        </div>
+                    </div>`
+                    // append card to the display weather div
+                    displayWeather.appendChild(dailyWeatherResults);
+                    // add bootsrtap classes to daily results div
+                    dailyWeatherResults.setAttribute("class", "row d-flex justify-content-center");
                 }
             });
 		} else {
